fff35d4b10f05686d68a81b10bd82f566b4e22ec
Olga Tichonova <tichonova.olga93@gmail.com>
2020-03-13 13:59:13 -0300

    Checkpoint 12.15 exercise

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,5 +1,5 @@
 {
   /*Insert the album title into the #albut-title element*/
   $('#album-title').text(album.title);
+  /* use .attr() method to add album.albumArtUrl to the src attribute of the img#album-cover-art element:*/
   $('img#album-cover-art').attr('src', album.albumArtUrl);
-}
