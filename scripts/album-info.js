b1690c751ea17fcb72a4626d897a9a6ff01c3ccd
Olga Tichonova <tichonova.olga93@gmail.com>
2020-03-13 14:02:12 -0300

    Checkpoint 12.15Assignment album-info.js: completed

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -2,7 +2,7 @@
   /*Insert the album title into the #albut-title element*/
   $('#album-title').text(album.title);
   /* use .attr() method to add album.albumArtUrl to the src attribute of the img#album-cover-art element:*/
-  $('img#album-cover-art').attr('src', album.albumArtUrl);
+  $('#album-cover-art').attr('src', album.albumArtUrl);
   /* mapping the artist */
   $('.artist').text(album.artist);
   /* mapping the release data */
