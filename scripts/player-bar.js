539a6f2e674e7d2cdc7ffc5fa583b5112d9c9aef
Olga Tichonova <tichonova.olga93@gmail.com>
2020-03-20 16:03:03 -0300

    Checkpoint 12.18 Exercise:complete

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -44,7 +44,7 @@ $('#time-control input').on('input', function (event) {
     const currentTime = player.getTime();
     const duration = player.getDuration();
     const percent = (currentTime / duration) * 100;
-    $('time-control .current-time').text( currentTime );
+    $('#time-control .current-time').text( currentTime );
     $('#time-control input').val(percent);
   }, 1000);
 }
